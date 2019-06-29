98d31060dd397e12c692f606c102f4193a73bd1b
kokanmalenko <kokan_malenko@hotmail.com>
2019-06-29 21:06:38 +0200

    Fix: imports and require context

---
+++ storybook-static/precache-manifest.f0b1a7e39b005097906b4784c2dbfbfd.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc5363d903c1c1eb5268f6694ef8813f",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.6951642a3406511c21d3.bundle.js"
+  },
+  {
+    "revision": "ccb2d42ebef909250a7a",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.6951642a3406511c21d3.bundle.js"
+  },
+  {
+    "url": "vendors~main.6951642a3406511c21d3.bundle.js"
+  }
+]);
